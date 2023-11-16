7acee8790e43aca6a8b61f3d85939359414f8ffb
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-16 00:44:23 -0600

    Created basic functionality for calculator

--- script.js
+++ script.js
@@ -19,13 +19,11 @@ updateDisplay()
 const keys = document.querySelector('.keys')
 keys.addEventListener('click', (event)=> {
     const {target} = event
-
-
     if(!target.matches('button')) {
         return
     }
 
-    if(target.classList.contains(operator)) {
+    if(target.classList.contains('operator')) {
         handleOperator(target.value)
         updateDisplay()
         return
@@ -97,3 +95,17 @@ const handleOperator = (nextOperator) => {
     calculator.operator = nextOperator
 }
 
+//Calculation logic
+
+const calculate = (firstOperand, secondOperand, operator) => {
+    if(operator === '+') {
+        return firstOperand + secondOperand
+    } else if(operator === "-") {
+        return firstOperand - secondOperand 
+    } else if(operator === "*") {
+        return firstOperand * secondOperand
+    } else if(operator === '/') {
+        return firstOperand / secondOperand
+    }
+    return secondOperand
+}
